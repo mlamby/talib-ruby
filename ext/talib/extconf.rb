f18544a02ebdafde5c408d4d9a3244292d5bcf93
Michael Lamb <mr.lamby@gmail.com>
2012-11-04 15:28:51 +1100

    Build talib before building makefile.

--- ext/talib/extconf.rb
+++ ext/talib/extconf.rb
@@ -1,6 +1,34 @@
 require 'mkmf'
 
-dir_config("talib")
+def sys(cmd)
+  puts " -- #{cmd}"
+  unless ret = xsystem(cmd)
+    raise "ERROR: '#{cmd}' failed"
+  end
+  ret
+end
 
-have_library("ta_lib", "TA_Initialize")
+CWD = File.expand_path(File.dirname(__FILE__))
+VENDOR_DIR = File.join(CWD, '..', '..', 'vendor')
+TARBALL = File.join(VENDOR_DIR, "ta-lib-0.4.0-src.tar.gz")
+TA_LIB_DIR = File.join(CWD, 'ta-lib')
+
+# Build the ta-lib library
+sys("tar -zxf #{TARBALL}")
+Dir.chdir('ta-lib') do
+  sys("./configure")
+  sys("make")
+  # Remove the .so files so that gcc wont be tempted to use
+  # them instead of the the .a files.
+  sys("rm src/.libs/*.so*")
+end
+
+$INCFLAGS[0,0] = " -I#{File.join("ta-lib", "include")} "
+$LDFLAGS << " -L#{File.join("ta-lib", "src", ".libs")} "
+
+unless have_library("ta_lib")
+  message "ta_lib library compilation failed\n"
+  abort
+end
+
 create_makefile("talib")
